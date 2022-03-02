8965d6f64a3e4a252f26fc7e862f49b64dbcc2df
TartaricAcid <baka943@qq.com>
2022-03-02 21:32:53 +0800

    Add github action

---
+++ pack.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import json
+
+
+def json_to_lang(project_in, namespace_in, lang_file):
+    lang_file_path = "./i18n/{}/assets/{}/lang/{}".format(project_in, namespace_in, lang_file)
+    with open(lang_file_path, "r", encoding="utf-8") as file:
+        lang_dict = json.load(file)
+    if lang_file.endswith(".json") and os.path.isfile(lang_file_path):
+        tmp_lang_file_path = "./tmp/{}/assets/{}/lang/{}.lang".format(project_in, namespace_in, lang_file[:-5])
+        with open(tmp_lang_file_path, "w", encoding="utf-8") as tmpFile:
+            for key, value in lang_dict.items():
+                tmpFile.writelines("{}={}\n".format(key, value))
+
+
+if __name__ == '__main__':
+    if not os.path.isdir("./tmp"):
+        os.makedirs("./tmp")
+    if not os.path.isdir("./out"):
+        os.makedirs("./out")
+    for folder in os.listdir("./files"):
+        shutil.copytree("./files/{}".format(folder), "./tmp/{}".format(folder))
+    for project in os.listdir("./i18n"):
+        projectAssetsPath = "./i18n/{}/assets".format(project)
+        if not os.path.isdir(projectAssetsPath):
+            continue
+        for namespace in os.listdir(projectAssetsPath):
+            langFolderPath = "./i18n/{}/assets/{}/lang".format(project, namespace)
+            if not os.path.isdir(langFolderPath):
+                continue
+            i18nFolderPath = "./i18n/{}/assets/{}/lang".format(project, namespace)
+            if not os.path.isdir(i18nFolderPath):
+                os.makedirs(i18nFolderPath)
+            for langFile in os.listdir(langFolderPath):
+                if langFile != "en_us.json":
+                    json_to_lang(project, namespace, langFile)
+    for project in os.listdir("./tmp"):
+        outPath = "./out/{}".format(project)
+        projectPath = "./tmp/{}/".format(project)
+        shutil.make_archive(outPath, "zip", projectPath)
+    shutil.rmtree("./tmp")
