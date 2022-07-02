8fc31f673bc0b10b5becbb565ce7f23b3eea6646
TartaricAcid <baka943@qq.com>
2022-07-02 19:45:44 +0800

    fix the error

--- docs/js/index.js
+++ docs/js/index.js
@@ -57,7 +57,7 @@ var vm = new Vue({
     },
     mounted() {
         axios
-            .get('/data/info.json')
+            .get('data/info.json')
             .then(response => {
                 this.all = response.data;
                 this.info = this.all;
